7802e78427a9475cbe5a1c19040f608f98071896
Andreas Lind <andreas.lind@peakon.com>
2019-02-08 12:01:38 +0100

    Fix preferredWidth setting

--- expect.js
+++ expect.js
@@ -1,8 +1,8 @@
 import unexpected from 'unexpected'
 import unexpectedImmutable from './lib/unexpected-immutable'
 
+unexpected.output.preferredWidth = 150
 const expect = unexpected.clone()
   .use(unexpectedImmutable)
-unexpected.output.preferredWidth = 150
 
 export default expect
